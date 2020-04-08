1815fe4ce3a7eeb407a70ef28ce5911a0eb31f8e
paulapivat <paul.apivat@gmail.com>
2020-04-08 10:06:25 +0700

    add treemap

--- R/analysis.R
+++ R/analysis.R
@@ -8,6 +8,7 @@ library(tidyverse)
 library(tibble)
 library(treemap)
 library(ggridges)
+library(RColorBrewer)
 
 # loading data
 df <- read.csv("/Users/paulapivat/Desktop/data_go_th/thai_scholarship.csv")
@@ -37,3 +38,50 @@ df3$granted_plus_purpose <- rowSums(df3[,c("granted", "purpose_total")], na.rm =
 
 # save back to df2
 df2 <- df3
+
+### Tidy data ###
+# note: we would not need ‘granted’, ‘granted_plus_purpose’ or ‘percentage’ for tidy data - 
+# it could be easily calculated (create new data frame df2a, but do not select unneeded columns)
+
+# select subset of data frame to separate “scholarships” from “purpose”
+
+# scholarship
+df2 %>%
+    select(id, countries, KING, CSC, MFA, MOST, MOPH, HEC, ODOS, IPST) -> df2a
+
+# purpose
+df2 %>%
+    select(id, countries, purpose_study, purpose_training, purpose_other) -> df2b
+
+# to create two new columns - scholarships and grants
+df2a <- df2a %>%
+    gather(`KING`, `CSC`, `MFA`, `MOST`, `MOPH`, `HEC`, `ODOS`, `IPST`, key = "scholarship_provider", value = "number_recipient")
+
+df2b <- df2b %>%
+    gather(`purpose_study`, `purpose_training`, `purpose_other`, key = "purpose", value = "number_people")
+
+# join (create new data frame to separate from two previous one)
+library(tidyverse)
+df2c <- left_join(df2a, df2b)
+
+#### TreeMap ####
+library(treemap)
+
+tree <- treemap(df2c, 
+	index = c("scholarship_provider", "countries"), 
+	vSize = "number_recipient", 	
+	type = "index", 
+	fontsize.labels = c(18,12), 
+	fontcolor.labels = c("black", "white"), 
+	bg.labels = c("transparent"), 
+	inflate.labels = FALSE, 
+	border.col = c("black", "white"), 
+	border.lwds = c(7,2), 
+	palette = "Set2"
+)
+
+### Barchart
+## Number of Scholarships granted by Type of Scholar (filled with ‘purpose’)
+
+
+
